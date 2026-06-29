f93b667e7705e4ec9e51cd433be87bbbe7f9f704
Armaan Sharma <armaan2345sharma@gmail.com>
2026-06-29 08:10:35 +0000

    learning array

---
+++ Arry/P1.py
@@ -0,0 +1,11 @@
+from array import *
+a1= array('i',[1,67,90,87])
+print(type(a1))
+print("for statements")
+for x in a1:
+    print(x)
+print("while statements")
+k=0
+while (k<len(a1)):
+    print(a1[k])
+    k+=1
