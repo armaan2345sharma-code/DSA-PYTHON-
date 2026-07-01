956bdbef775f0981ac0736f60910ab3597171d03
Armaan Sharma <armaan2345sharma@gmail.com>
2026-07-01 10:52:42 +0000

    revisig classes

---
+++ class and /class.py
@@ -0,0 +1,8 @@
+class Test:
+    T1=5
+    print("hello")
+    def f1(self):
+        print("this is f1 method")
+t1=Test()#instance object of class Test
+t1.f1()#calling method f1() on instance object t1
+ 
