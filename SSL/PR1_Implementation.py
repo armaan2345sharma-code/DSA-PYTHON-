47175c01985257e99751162d50370ec30db0710f
Armaan Sharma <armaan2345sharma@gmail.com>
2026-07-08 03:04:55 +0000

    self done linked list implementation

--- SSL/PR1_Implementation.py
+++ SSL/PR1_Implementation.py
@@ -15,9 +15,19 @@ class sll:
             self.next = sll()
         else:
             self.next.insert_end(item)
+    def search(self,item):
+        if self.is_empty():
+            return False
+        elif self.item == item:
+            return True
+        else:
+            return False
+        
+
 t3=sll(3)
 t2=sll(2,t3)
 t1=sll(1,t2)
 t0=sll(0,t1)
 print("The linked list is:")
 print(t0.list_print())
+print("Search for 2:", t0.search(2))
