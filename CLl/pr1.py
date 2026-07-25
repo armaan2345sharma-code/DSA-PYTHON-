f28c546621d94cb22daa501d085515acae8b480c
Armaan Sharma <armaan2345sharma@gmail.com>
2026-07-25 18:30:29 +0000

    qqad

--- CLl/pr1.py
+++ CLl/pr1.py
@@ -1,2 +1,11 @@
 class node:
-    
+    def __init__(self,data=None,next=None):
+        self.data=data 
+        self.next=next
+class cll:
+    def __init__(self, start=None):
+        self.start=start
+    def is_empty(self):
+        return self.start is None
+    def at_start(self):
+        
