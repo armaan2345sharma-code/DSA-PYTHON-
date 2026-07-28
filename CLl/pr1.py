fe594efde1333c431924a5248ac7d735c1123b98
Armaan Sharma <armaan2345sharma@gmail.com>
2026-07-28 17:03:07 +0000

    making insert_start command

--- CLl/pr1.py
+++ CLl/pr1.py
@@ -7,6 +7,20 @@ class cll:
         self.start=start
     def is_empty(self):
         return self.start is None
-    def at_start(self):
-        n=node(data,self.start)
+    def insert_start(self,data):
+        n=node(data)
+        if self.start is not None:
+            n.next=self.start
+        self.start=n
+    def print_list(self):
+        temp=self.start
+        while temp is not None:
+            print(temp.data,end=" ")
+            temp=temp.next
+#driver code
+k=cll()
+k.insert_start(25)
+k.print_list()
+    
+
         
