f71ecbded4d9f326aeefb8754b7c68c1f892894d
Armaan Sharma <armaan2345sharma@gmail.com>
2026-07-29 16:29:27 +0000

    making cll

--- CLl/pr1.py
+++ CLl/pr1.py
@@ -12,6 +12,16 @@ class cll:
         if self.start is not None:
             n.next=self.start
         self.start=n
+    def insert_last(self,data):
+        n=node(data)
+        temp=self.start
+        if temp.next is None:
+            temp.next=n
+            temp=temp.next
+    def cll(self):
+        temp=self.start
+        while temp.next==None:
+            temp.next=self.start
     def print_list(self):
         temp=self.start
         while temp is not None:
@@ -20,6 +30,8 @@ class cll:
 #driver code
 k=cll()
 k.insert_start(25)
+k.insert_last(30)
+k.cll()
 k.print_list()
     
 
