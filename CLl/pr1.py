6512ba8108b46d82010f4ded6851429f4a92a955
Armaan Sharma <armaan2345sharma@gmail.com>
2026-07-30 13:43:42 +0000

    cll

--- CLl/pr1.py
+++ CLl/pr1.py
@@ -20,17 +20,43 @@ class cll:
             temp=temp.next
     def cll(self):
         temp=self.start
-        while temp.next==None:
-            temp.next=self.start
-    def print_list(self):
+        while temp.next is not None:
+            temp=temp.next
+        temp.next=self.start
+    def search(self,data):
         temp=self.start
-        while temp is not None:
+        while True:
+            if temp.data == data:
+                print ("y")
+                return temp
+            temp=temp.next
+            if temp==self.start:
+                break
+        return None
+                
+    def insert_after(self,data,key):
+        temp=self.search(key)
+        n=node(data)
+        n.next=temp.next
+        temp.next=n
+
+            
+
+    def print_list(self):
+        if self.start is None:
+            return
+        temp=self.start
+        while True:
             print(temp.data,end=" ")
+            if temp.next is self.start:
+                break
             temp=temp.next
 #driver code
 k=cll()
 k.insert_start(25)
 k.insert_last(30)
+k.search(25)
+k.insert_after(38,30)
 k.cll()
 k.print_list()
     
