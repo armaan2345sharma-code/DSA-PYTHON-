d12595e3d4222a89938bd3f0f99ae01a74ec809b
Armaan Sharma <armaan2345sharma@gmail.com>
2026-08-16 14:12:35 +0530

    cdl

--- cdl/pr1.py
+++ cdl/pr1.py
@@ -4,11 +4,12 @@ class node:
        self.next=next
        self.prev=prev
 class cdl:
-    def _init_(self):
+    def __init__(self):
         self.start=None
     def list_empty(self):
         if self.start==None:
             return True
+        return False
     def insert_start(self,data):
         n=node(data)
         if self.list_empty():
@@ -21,6 +22,27 @@ class cdl:
             self.start.prev.next=n
             self.start.prev=n
             self.start=n
+    def 
+    def priint(self):
+        temp=self.start
+        if self.list_empty():
+            pass
+        else:
+            while True:
+                temp=temp.next
+                print(temp.data," ")
+                if temp==self.start:
+                    break
+#Trial code 
+k=cdl()
+k.insert_start(10)
+k.insert_start(20)      
+
+k.priint()
+
+            
+
+
 
 
             
