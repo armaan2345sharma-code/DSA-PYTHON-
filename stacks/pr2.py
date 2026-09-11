bd5cd1af9675a2ba22d139091fdaac7a492256f1
Armaan Sharma <armaan2345sharma@gmail.com>
2026-09-11 18:41:33 +0530

    making stacks by list

--- stacks/pr2.py
+++ stacks/pr2.py
@@ -5,6 +5,11 @@ class stacks:
         return len(self.items)==0
     def push(self,data):
         self.items.append(data)
+    def peak(self):
+        return self.items[-1]
+        
+    def pop(self):
+        self.items.pop()
     def printL(self):
         print(self.items)
 k=stacks()
@@ -12,4 +17,7 @@ k.push(45)
 k.push(56)
 k.push(56)
 k.push(90)
+print(k.peak())
+k.pop()
+
 k.printL()
